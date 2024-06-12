1e09a77e44be1bed69b7f70c9727cb520fce8abd
Gligor Biljarski <gbiljarski@icloud.com>
2024-06-12 16:11:15 +0200

    some changes

--- GligorCoreProject.Web/Controllers/HomeController.cs
+++ GligorCoreProject.Web/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace GligorCoreProject.Web.Controllers
             return View();
         }
 
-       
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
