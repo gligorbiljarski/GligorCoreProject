fb720e0d4e6a7c141940e3cf5673cc14f9c3a00a
F5tech <F5tech@DESKTOP-VU5358V>
2024-06-12 16:02:58 +0200

    Add project files.

---
+++ GligorCoreProject.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using GligorCoreProject.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GligorCoreProject.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Gligor Programerot",
+                DateOfBirth = new DateTime(1987,08,11)
+            };
+            return View(data);
+        }
+    }
+}
